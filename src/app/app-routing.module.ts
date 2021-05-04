3968488b783d11243fc8b94af380c055556b9d20
LAymeric <aymeric.larvet@gmail.com>
2021-05-04 22:25:17 +0200

    connexion page init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouteReuseStrategy, RouterModule, Routes} from '@angular/router';
 import {NoMatchRedirectGuardService} from './core/guards/no-match-redirect-guard.service';
 import {HomeRootComponent} from './public/home/home-root/home-root.component';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -10,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), FormsModule, ReactiveFormsModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
--- src/app/public/login/login-root/login-root.component.ts
+++ src/app/public/login/login-root/login-root.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-root',
@@ -7,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class LoginRootComponent implements OnInit {
+    form: FormGroup;
+    loading = false; // so no multiple clicks
+    submitted = false; // know if submitted yet to display errors
 
-  constructor() { }
+    constructor(
+        private formBuilder: FormBuilder,
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            username: [null, Validators.required],
+            password: [null, Validators.required]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        alert("submit")
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        // todo login happens
+    }
 
 }
