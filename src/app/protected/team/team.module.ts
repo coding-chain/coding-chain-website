0b0a8c451829a25b6f5405f88dd2c7623de584ca
LAymeric <aymeric.larvet@gmail.com>
2021-05-11 18:10:46 +0200

    improving team page

--- src/app/protected/team/team.module.ts
+++ src/app/protected/team/team.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import {TeamRootComponent} from './team-root/team-root.component';
 import {TeamPageComponent} from './team-page/team-page.component';
 import {TeamRoutingModule} from './team-routing.module';
+import {TeamFormComponent} from './team-form/team-form.component';
+import {SharedModule} from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     TeamRootComponent,
-    TeamPageComponent
+    TeamPageComponent,
+    TeamFormComponent
   ],
   imports: [
     CommonModule,
-    TeamRoutingModule
+    TeamRoutingModule,
+    SharedModule
   ]
 })
 export class TeamModule { }
