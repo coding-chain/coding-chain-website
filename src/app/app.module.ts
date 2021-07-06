730eee6ecaeb35651c3b94f6f9af03680494b4d0
BAILLEUL <bailleul.sacha@gmail.com>
2021-07-06 15:32:48 +0200

    [chore]: Change app route url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {registerLocaleData} from '@angular/common';
+import {APP_BASE_HREF, registerLocaleData} from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import {CoreModule} from './core/core.module';
 import {PublicModule} from './public/public.module';
 import {ProtectedModule} from './protected/protected.module';
 import {AppRoutingModule} from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { UsersRootComponent } from './protected/users/users-root/users-root.component';
-import { UsersListComponent } from './protected/users/users-list/users-list.component';
-import { UsersFilterComponent } from './protected/users/users-filter/users-filter.component';
 
 registerLocaleData(localeFr, 'fr');
 
@@ -29,7 +26,7 @@ registerLocaleData(localeFr, 'fr');
     AppRoutingModule,
     NgbModule,
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'fr-fr'}],
+  providers: [{provide: APP_BASE_HREF, useValue: '/web/'}, {provide: LOCALE_ID, useValue: 'fr-fr'}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
